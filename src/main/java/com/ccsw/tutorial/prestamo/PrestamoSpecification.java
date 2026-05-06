4bb77cd8fb9b97d7f335406fa1df591d58153a40
Carmen Crespo <135099060+neeem111@users.noreply.github.com>
2026-05-06 09:32:55 +0200

    specifi

--- src/main/java/com/ccsw/tutorial/prestamo/PrestamoSpecification.java
+++ src/main/java/com/ccsw/tutorial/prestamo/PrestamoSpecification.java
@@ -1,34 +1,32 @@
 package com.ccsw.tutorial.prestamo;
 
 import com.ccsw.tutorial.prestamo.model.Prestamo;
+import com.ccsw.tutorial.prestamo.model.PrestamoSearchDto;
 import jakarta.persistence.criteria.Predicate;
 import org.springframework.data.jpa.domain.Specification;
 
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Specification para filtros de prestamo
- */
-public class prestamoSpecification {
+public class PrestamoSpecification {
 
-    public static Specification<Prestamo> createSpecification(prestamoSearchDto dto) {
-
+    public static Specification<Prestamo> createSpecification(PrestamoSearchDto dto) {
         return (root, query, criteriaBuilder) -> {
-
             List<Predicate> predicates = new ArrayList<>();
 
             if (dto.getGameId() != null) {
-                predicates.add(criteriaBuilder.equal(root.get("game").get("id"), dto.getGameId()));
+                predicates.add(criteriaBuilder.equal(
+                        root.get("game").get("id"), dto.getGameId()));
             }
-
             if (dto.getClientId() != null) {
-                predicates.add(criteriaBuilder.equal(root.get("client").get("id"), dto.getClientId()));
+                predicates.add(criteriaBuilder.equal(
+                        root.get("client").get("id"), dto.getClientId()));
             }
-
             if (dto.getDate() != null) {
-
-                predicates.add(criteriaBuilder.and(criteriaBuilder.lessThanOrEqualTo(root.get("startDate"), dto.getDate()), criteriaBuilder.greaterThanOrEqualTo(root.get("endDate"), dto.getDate())));
+                predicates.add(criteriaBuilder.and(
+                        criteriaBuilder.lessThanOrEqualTo(root.get("startDate"), dto.getDate()),
+                        criteriaBuilder.greaterThanOrEqualTo(root.get("endDate"), dto.getDate())
+                ));
             }
 
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
