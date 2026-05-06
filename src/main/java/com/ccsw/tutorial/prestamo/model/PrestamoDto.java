d5082ee52f1e1c5d0ab6c2ab0b2c8791dc81b63f
Carmen Crespo <135099060+neeem111@users.noreply.github.com>
2026-05-06 09:50:08 +0200

    dto

--- src/main/java/com/ccsw/tutorial/prestamo/model/PrestamoDto.java
+++ src/main/java/com/ccsw/tutorial/prestamo/model/PrestamoDto.java
@@ -1,74 +1,32 @@
 package com.ccsw.tutorial.prestamo.model;
 
+import com.ccsw.tutorial.cliente.model.ClienteDto;
+import com.ccsw.tutorial.game.model.GameDto;
 import java.time.LocalDate;
 
 public class PrestamoDto {
 
     private Long id;
 
-    private Long clienteId;
-    private String clienteName;
-
-    private Long gameId;
-    private String gameTitle;
+    // Objetos anidados igual que GameDto tiene AuthorDto y CategoryDto
+    private ClienteDto client;
+    private GameDto game;
 
     private LocalDate startDate;
     private LocalDate endDate;
 
-    public Long getId() {
-        return this.id;
-    }
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    public ClienteDto getClient() { return client; }
+    public void setClient(ClienteDto client) { this.client = client; }
 
-    public Long getClientId() {
-        return this.clienteId;
-    }
+    public GameDto getGame() { return game; }
+    public void setGame(GameDto game) { this.game = game; }
 
-    public void setClientId(Long clientId) {
-        this.clienteId = clientId;
-    }
+    public LocalDate getStartDate() { return startDate; }
+    public void setStartDate(LocalDate startDate) { this.startDate = startDate; }
 
-    public String getClientName() {
-        return this.clienteName;
-    }
-
-    public void setClientName(String clientName) {
-        this.clienteName = clientName;
-    }
-
-    public Long getGameId() {
-        return this.gameId;
-    }
-
-    public void setGameId(Long gameId) {
-        this.gameId = gameId;
-    }
-
-    public String getGameTitle() {
-        return this.gameTitle;
-    }
-
-    public void setGameTitle(String gameTitle) {
-        this.gameTitle = gameTitle;
-    }
-
-    public LocalDate getStartDate() {
-        return this.startDate;
-    }
- 
-    public void setStartDate(LocalDate startDate) {
-
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return this.endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
+    public LocalDate getEndDate() { return endDate; }
+    public void setEndDate(LocalDate endDate) { this.endDate = endDate; }
 }
